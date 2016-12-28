b06ae232d7d4e40f11b2a13bb2f4ac940a2fb452
zia <zia@192.168.1.131>
2016-12-27 21:11:11 -0500

    Album module introduced.

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Zia Web LLC
+ */
+
+namespace Album\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+
+    public function indexAction()
+    {
+        
+    }
+
+    public function addAction()
+    {
+        
+    }
+
+    public function editAction()
+    {
+        
+    }
+
+    public function deleteAction()
+    {
+        
+    }
+
+}
---
+++ module/Album/src/Module.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Zia Web LLC
+ */
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements ConfigProviderInterface
+{
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+}
