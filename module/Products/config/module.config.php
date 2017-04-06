bdea5f341cb1f2bc953cc4d014af2b3687db180d
DESKTOP-HMPEIFC\sumon <dahukpakhi@gmail.com>
2017-04-06 15:41:56 +0600

    #sumon zf3skeleton Navigation menu adde but 404 error occured

--- module/Products/config/module.config.php
+++ module/Products/config/module.config.php
@@ -2,12 +2,31 @@
 
 namespace Products;
 
+use Zend\Router\Http\Segment;
 use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
     'controllers' => [
         'factories' => [
             Controller\AlbumController::class => InvokableFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'products' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/products[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\ProductsController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
         ],
     ],
     'view_manager' => [
---
+++ module/Products/src/Controller/ProductsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Products\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class ProductsController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
